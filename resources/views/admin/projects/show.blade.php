b378be71f15b2b9a52f4476c95625b51dd5e0feb
Igor-86 <i.campoli86@gmail.com>
2023-01-19 15:03:29 +0100

    first commit

---
+++ resources/views/admin/projects/show.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.admin')
+
+@section('content')
+    <div class="container">
+        <div class="row mt-4">
+            <div class="col-9">
+                <div class="type-project mb-1">
+                    @if ($project->type)
+                        <h4>{{ $project->type?->name }}</h4>
+                    @else
+                        <h4>Not type</h4>
+                    @endif
+                </div>
+                <div class="d-flex justify-content-between">
+                    <h3>{{ $project->title }}</h3>
+                    <div class="d-flex">
+                        <span>Creato il:</span>
+                        <p class="ms-4 color-date align-items-center">{{ $project->created_at }}</p>
+                    </div>
+                </div>
+                <div class="show-image mt-3">
+                    @if ($project->cover_image)
+                        <img src="{{ asset('storage/' . $project->cover_image) }}" alt="">
+                    @else
+                        <div class="show-not-image">
+                            Image not yet!
+                        </div>
+                    @endif
+                </div>
+                <div>
+                    <p class="mt-3">{{ $project->article }}</p>
+                </div>
+                <div class="tags mt-2">
+                    @forelse ($project->technologies as $technology)
+                        <span>#{{ $technology->name }}</span>
+                    @empty
+                        <span>Nessun tag</span>
+                    @endforelse
+                </div>
+                <div>
+                    <a class="btn btn-dark mt-3" href="{{ route('admin.projects.index') }}">Projects</a>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
